f731cb259b398b4c8234cf0f7e8d78dee1b978e9
180031079 <56549996+180031079@users.noreply.github.com>
2024-11-09 17:26:05 -0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,9 @@
+# Get input from the user
+first_number = input("Enter the first number: ")
+second_number = input("Enter the second number: ")
+
+# Calculate the sum
+sum_result = float(first_number) + float(second_number)
+
+# Print the result
+print("The sum is:", sum_result)
